6c92ff30905f330c84fc213e0cdd5275642af20a
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-23 00:03:59 +0300

    AtCoder - abc043_a

---
+++ At Coder/Children and Candies (ABC Edit).cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace problem_solving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine((n * (n + 1)) / 2);
+
+        }
+    }
+}
