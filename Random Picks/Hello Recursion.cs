ceda56dbaa642ae0a894cf8b43e5f7c9e9f565b8
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-23 01:36:17 +0300

    SPOJ - HRECURS

---
+++ Random Picks/Hello Recursion.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tests = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= tests; i++)
+            {
+                long[] ints = Console.ReadLine().Split(" ").Select(long.Parse).ToArray();
+                long x = Sum(ints, ints[0]);
+                Console.WriteLine($"Case {i}: {x}");
+            }
+
+        }
+        static long Sum(long[] nums, long i)
+        {
+            if (i == 1)
+                return nums[1];
+            else
+                return nums[i] + Sum(nums, i - 1);
+        }
+    }
+}
