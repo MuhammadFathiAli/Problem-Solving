611a8a8356dc2c3106b3b8c833f981d267da0a59
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-23 00:05:25 +0300

    CodeForces - 630A

---
+++ CodeForces/Again Twenty Five.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace problem_solving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            Console.WriteLine(25);
+        }
+    }
+}
