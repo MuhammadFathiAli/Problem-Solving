46a2910f7df223ca1c7beeafe16433cee16965a7
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-23 00:14:09 +0300

    AtCoder - abc161_a

---
+++ At Coder/ABC Swap.cs
@@ -0,0 +1,12 @@
+using System;
+using namespace std;
+
+int main()
+{
+    int x = int.Parse(Console.ReadLine());
+    int y = int.Parse(Console.ReadLine());
+    int z = int.Parse(Console.ReadLine());
+    Console.WriteLine($"{z} {x} {y}");
+}
+
+
