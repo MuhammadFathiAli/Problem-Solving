05f575ef7117d2390743957e4fc73190df5356a9
MuhammadFathiAli <muhammad.fathi64@gmail.com>
2023-06-23 01:37:37 +0300

    HackerRank - ctci-fibonacci-numbers

---
+++ HackerRanck/Recursion_Fibonacci Numbers.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.IO;
+
+class Solution
+{
+
+    public static int Fibonacci(int n)
+    {
+        if (n == 0)
+            return 0;
+        else if (n == 1 || n == 2)
+            return 1;
+        else
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+
+    static void Main(String[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Fibonacci(n));
+    }
+}
